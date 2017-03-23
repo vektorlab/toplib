093db92eb6b8d64150adff8d7be63dc5a4dbb33b
Kevin Schoon <kevinschoon@gmail.com>
2017-03-23 18:21:44 +0700

    update mock example

--- examples/mock.go
+++ examples/mock.go
@@ -23,56 +23,62 @@ func RandString(n int) string {
 }
 
 type MockSource struct {
-	samples map[sample.Namespace][]*sample.Sample
+	samples   []*sample.Sample
+	namespace sample.Namespace
 }
 
 func (m *MockSource) Collect() ([]*sample.Sample, error) {
-	results := []*sample.Sample{}
-	for _, samples := range m.samples {
-		for _, smpl := range samples {
-			// Uncomment to change iteration size
-			if rand.Intn(10) > 5 {
-				continue
-			}
-
-			s := sample.NewSample(smpl.ID(), smpl.Namespace())
-			s.SetFloat64("CPU", float64(rand.Intn(5)))
-			s.SetFloat64("MEM", float64(rand.Intn(40)))
-			s.SetFloat64("DISK", float64(rand.Intn(60)))
-			s.SetFloat64("GPU", float64(rand.Intn(90)))
-			s.SetString("THING", RandString(50))
-			s.SetString("OTHER THING", RandString(20))
-			results = append(results, s)
-		}
+	samples := []*sample.Sample{}
+	for _, seed := range m.samples {
+		// Uncomment to change iteration size
+		//if rand.Intn(10) > 5 {
+		//	continue
+		//}
+		s := sample.NewSample(seed.ID(), m.namespace)
+		s.SetFloat64("CPU", float64(rand.Intn(5)))
+		s.SetFloat64("MEM", float64(rand.Intn(40)))
+		s.SetFloat64("DISK", float64(rand.Intn(60)))
+		s.SetFloat64("GPU", float64(rand.Intn(90)))
+		s.SetString("THING", RandString(50))
+		s.SetString("OTHER THING", RandString(20))
+		samples = append(samples, s)
 	}
-	return results, nil
+	return samples, nil
 }
 
 func main() {
-	namespaces := []sample.Namespace{
-		sample.Namespace("ns-0"),
-		sample.Namespace("ns-1"),
-		sample.Namespace("ns-2"),
-	}
-	source := &MockSource{
-		samples: map[sample.Namespace][]*sample.Sample{
-			namespaces[0]: []*sample.Sample{},
-			namespaces[1]: []*sample.Sample{},
-			namespaces[2]: []*sample.Sample{},
+	sources := []*MockSource{
+		&MockSource{
+			namespace: sample.Namespace("ns-0"),
+			samples:   []*sample.Sample{},
+		},
+		&MockSource{
+			namespace: sample.Namespace("ns-1"),
+			samples:   []*sample.Sample{},
+		},
+		&MockSource{
+			namespace: sample.Namespace("ns-2"),
+			samples:   []*sample.Sample{},
 		},
 	}
 	for i := 0; i < 35; i++ {
-		source.samples[namespaces[0]] = append(source.samples[namespaces[0]], sample.NewSample(RandString(10), namespaces[0]))
-		source.samples[namespaces[1]] = append(source.samples[namespaces[1]], sample.NewSample(RandString(10), namespaces[1]))
-		source.samples[namespaces[2]] = append(source.samples[namespaces[2]], sample.NewSample(RandString(10), namespaces[2]))
+		sources[0].samples = append(sources[0].samples, sample.NewSample(RandString(10), sources[0].namespace))
+		sources[1].samples = append(sources[1].samples, sample.NewSample(RandString(10), sources[1].namespace))
+		sources[2].samples = append(sources[2].samples, sample.NewSample(RandString(10), sources[2].namespace))
 	}
 	sections := []toplib.Section{
-		section.NewSamples(namespaces[0], "ID", "CPU", "MEM", "DISK", "GPU"),
-		section.NewSamples(namespaces[1], "ID", "CPU", "MEM", "DISK", "GPU", "THING"),
-		section.NewSamples(namespaces[2], "ID", "CPU", "MEM", "DISK", "GPU", "THING", "OTHER THING"),
-		&section.Debug{Namespaces: namespaces},
+		section.NewSamples(sources[0].namespace, "ID", "CPU", "MEM", "DISK", "GPU"),
+		section.NewSamples(sources[1].namespace, "ID", "CPU", "MEM", "DISK", "GPU", "THING"),
+		section.NewSamples(sources[2].namespace, "ID", "CPU", "MEM", "DISK", "GPU", "THING", "OTHER THING"),
+		&section.Debug{
+			Namespaces: []sample.Namespace{
+				sources[0].namespace,
+				sources[1].namespace,
+				sources[2].namespace,
+			},
+		},
 	}
-	if err := toplib.Run(toplib.NewTop(sections), source.Collect); err != nil {
+	if err := toplib.Run(toplib.NewTop(sections), sources[0].Collect, sources[1].Collect, sources[2].Collect); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
